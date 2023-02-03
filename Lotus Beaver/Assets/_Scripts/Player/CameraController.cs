9a9ad09471f6afaac02ca4b73a671bcbbb7f8760
Yannik <yannik.braendle@t-online.de>
2023-02-03 23:58:23 +0100

    character movement and camera control added

---
+++ Lotus Beaver/Assets/_Scripts/Player/CameraController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private MovementSettings _movementSettings;
+    private float _lastDamping;
+    private CinemachineVirtualCamera _vcam;
+    private CinemachineHardLockToTarget _hardLockToTarget;
+    private void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        PlayerMovement playerMovement = player.GetComponent<PlayerMovement>();
+        
+        _vcam = GetComponent<CinemachineVirtualCamera>();
+        _hardLockToTarget = _vcam.GetCinemachineComponent<CinemachineHardLockToTarget>();
+        if (player != null)
+        {
+            _vcam.Follow = playerMovement.CameraTarget;
+            _vcam.LookAt = playerMovement.CameraTarget;
+        }
+        _lastDamping = -1;
+    }
+
+    private void Update() {
+        if(_lastDamping != _movementSettings.cameraDamping)
+        {
+            _hardLockToTarget.m_Damping = _movementSettings.cameraDamping;
+            _lastDamping = _movementSettings.cameraDamping;
+        }
+    }
+}
---
+++ Lotus Beaver/Assets/_Scripts/Player/MovementSettings.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MovementSettings", menuName = "Lotus Beaver/MovementSettings", order = 0)]
+public class MovementSettings : ScriptableObject {
+    [Header("Movement")]
+    [Range(0.01f,10f)] public float moveSpeed = 1f;
+    [Range(0.01f,1f)] public float acceleration = 0.7f;
+
+    [Header("Camera")]
+    [Range(0f,10f)] public float cameraDistance = 1f;
+    [Range(0f,10f)] public float cameraDamping = 1f;
+}
