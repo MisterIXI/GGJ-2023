5762f7b2edc6e93ce16c36260ca66067402a9132
JonasGruetzmacher <61141274+JonasGruetzmacher@users.noreply.github.com>
2023-02-04 16:04:14 +0100

    buildings

---
+++ Lotus Beaver/Assets/_Scripts/Buildings/BuildingPreset.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "Building Preset", menuName = "ScriptableObjects/New Building Preset")]
+public class BuildingPreset : ScriptableObject
+{
+    [field:SerializeField] public string displayName { get; private set; }
+
+    [field: SerializeField] public int earthCost { get; private set; }
+    [field: SerializeField] public int waterCost { get; private set; }
+
+    [field: SerializeField] public GameObject buildingPrefab { get; private set; }
+
+    [field: SerializeField] public int earthProduction { get; private set; }
+    [field: SerializeField] public int waterProduction { get; private set; }
+}
