a7bc975726d77a532e65097e215e924fe15ced63
Yannik <yannik.braendle@t-online.de>
2023-02-04 16:55:29 +0100

    Building preparation

--- Lotus Beaver/Assets/_Scripts/Interactions/BuildingInteractionSettings.cs
+++ Lotus Beaver/Assets/_Scripts/Interactions/BuildingInteractionSettings.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "BuildingInteractionSettings", menuName = "Lotus Beaver/BuildingInteractionSettings", order = 0)]
-public class BuildingInteractionSettings : ScriptableObject {
+public class BuildingInteractionSettings : ScriptableObject
+{
     public int earthCost = 0;
     public int waterCost = 0;
     // public Building building;
-    public GameObject buildingPrefab;
+    public InteractableBase buildingPrefab;
 }
--- Lotus Beaver/Assets/_Scripts/Interactions/EarthInteraction.cs
+++ Lotus Beaver/Assets/_Scripts/Interactions/EarthInteraction.cs
@@ -6,6 +6,7 @@ public class EarthInteraction : IInteractable
     public void OnInteract(Tile tile)
     {
         TileManager.SetTileElementType(tile, TileElementType.Earth);
+
     }
 
     public void OnSelection(Tile tile)
